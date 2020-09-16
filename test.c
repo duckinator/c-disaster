00cf7ae5fb98aec73f216f4fe6e6df536d496425
Ellen Marie Dash <me@duckie.co>
2020-09-16 17:09:34 -0400

    omg it works holy fucking shit

--- test.c
+++ test.c
@@ -2,47 +2,43 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define count1 0
-#define count2 count1,0
-#define count3 count2,0
-#define count4 count3,0
-#define count5 count4,0
-#define count6 count5,0
-#define count7 count6,0
-#define count8 count7,0
-#define count9 count8,0
-#define count10 count9,0
-#define count11 count10,0
+#define TypeToId(e) _Generic(e, \
+                    char*:      2, \
+                    char:       3, \
+                    int8_t:     4, \
+                    uint8_t:    5, \
+                    int16_t:    6, \
+                    uint16_t:   7, \
+                    int32_t:    8, \
+                    uint32_t:   9, \
+                    int64_t:   10, \
+                    uint64_t:  11, \
+                    default:    1)
 
-#define _data {count1}
-#define _str  {count2}
-#define _chr  {count3}
-#define _i8   {count4}
-#define _u8   {count5}
-#define _i16  {count6}
-#define _u16  {count7}
-#define _i32  {count8}
-#define _u32  {count9}
-#define _i64  {count10}
-#define _u64  {count11}
+static int _data[TypeToId((void*)1)]    = {1};
+static int _str [TypeToId((char*)1)]    = {2};
+static int _chr [TypeToId((char)1)]     = {3};
+static int _i8  [TypeToId((int8_t)1)]   = {4};
+static int _u8  [TypeToId((uint8_t)1)]  = {5};
+static int _i16 [TypeToId((int16_t)1)]  = {6};
+static int _u16 [TypeToId((uint16_t)1)] = {7};
+static int _i32 [TypeToId((int32_t)1)]  = {8};
+static int _u32 [TypeToId((uint32_t)1)] = {9};
+static int _i64 [TypeToId((int64_t)1)]  = {10};
+static int _u64 [TypeToId((uint64_t)1)] = {11};
 
-#define Value(e) _Generic(  &((int[])e.type),\
-                            int(*)[ 1]: either_getfn_data,  \
-                            int(*)[ 2]: either_getfn_str,   \
-                            int(*)[ 3]: either_getfn_chr,   \
-                            int(*)[ 4]: either_getfn_i8,    \
-                            int(*)[ 5]: either_getfn_u8,    \
-                            int(*)[ 6]: either_getfn_i16,   \
-                            int(*)[ 7]: either_getfn_u16,   \
-                            int(*)[ 8]: either_getfn_i32,   \
-                            int(*)[ 9]: either_getfn_u32,   \
-                            int(*)[10]: either_getfn_i64,   \
-                            int(*)[11]: either_getfn_u64)(e)
-
-#define EitherStruct(TYPE, DATA, OKAY) ((struct { union SourceOfAnxiety as; bool is_okay; int type[]; }){.as.TYPE=DATA, OKAY, _##TYPE})
-
-#define Okay(TYPE, DATA) ({.as.TYPE=DATA, true,  _##TYPE}
-#define Err(TYPE, DATA)  {.as.TYPE=DATA, false, _##TYPE}
+#define Value(e) _Generic( (&(e.type)),\
+                            int(*)[ 1]: e.as.data,  \
+                            int(*)[ 2]: e.as.str,   \
+                            int(*)[ 3]: e.as.chr,   \
+                            int(*)[ 4]: e.as.i8,    \
+                            int(*)[ 5]: e.as.u8,    \
+                            int(*)[ 6]: e.as.i16,   \
+                            int(*)[ 7]: e.as.u16,   \
+                            int(*)[ 8]: e.as.i32,   \
+                            int(*)[ 9]: e.as.u32,   \
+                            int(*)[10]: e.as.i64,   \
+                            int(*)[11]: e.as.u64)
 
 union SourceOfAnxiety {
     void    *data;
@@ -58,38 +54,42 @@ union SourceOfAnxiety {
     uint64_t u64;
 };
 
+#define EitherStruct(TYPE, DATA) \
+    struct { union SourceOfAnxiety as; bool is_okay; int type[TypeToId(DATA)]; }
+
+
+#define _TypeDisaster(TYPE) _Generic( (&(_##TYPE)),\
+    int(*)[ 1]: (void*)1,  \
+    int(*)[ 2]: (char*)1,   \
+    int(*)[ 3]: (char)1,   \
+    int(*)[ 4]: (int8_t)1,    \
+    int(*)[ 5]: (uint8_t)1,    \
+    int(*)[ 6]: (int16_t)1,   \
+    int(*)[ 7]: (uint16_t)1,   \
+    int(*)[ 8]: (int32_t)1,   \
+    int(*)[ 9]: (uint32_t)1,   \
+    int(*)[10]: (int64_t)1,   \
+    int(*)[11]: (uint64_t)1)
+
+#define Either(TYPE) EitherStruct(TYPE, _TypeDisaster(TYPE))
+
+#define Okay(TYPE, DATA) {.as.TYPE=DATA, .is_okay=true, .type={0}}
+#define Err(TYPE, DATA)  {.as.TYPE=DATA, .is_okay=false, .type={0}}
+
 typedef struct either_s {
     union SourceOfAnxiety as;
     bool is_okay;
-    int type[];
-} Either;
-
-void    *either_getfn_data(Either e) { printf("data\n"); return e.as.data; }
-char    *either_getfn_str (Either e) { printf("str\n"); return e.as.str;  }
-char     either_getfn_chr (Either e) { printf("chr\n"); return e.as.chr;  }
-int8_t   either_getfn_i8  (Either e) { printf("i8\n");  return e.as.i8;   }
-uint8_t  either_getfn_u8  (Either e) { printf("u8\n");  return e.as.u8;   }
-int16_t  either_getfn_i16 (Either e) { printf("i16\n"); return e.as.i16;  }
-uint16_t either_getfn_u16 (Either e) { printf("u16\n"); return e.as.u16;  }
-int32_t  either_getfn_i32 (Either e) { printf("i32\n"); return e.as.i32;  }
-uint32_t either_getfn_u32 (Either e) { printf("u32\n"); return e.as.u32;  }
-int64_t  either_getfn_i64 (Either e) { printf("i64\n"); return e.as.i64;  }
-uint64_t either_getfn_u64 (Either e) { printf("u64\n"); return e.as.u64;  }
+    int type[12];
+} Either_t;
 
 int main()
 {
-    Either x = (Either)(struct { union SourceOfAnxiety as; bool is_okay; int type[8]; }){.as.i32=1234, true, _i32};
+    //struct {union SourceOfAnxiety as; _Bool is_okay; int type[8];} a = { .as.i32=1234, .is_okay=1, .type={0}};
+    Either(i32) a = Okay(i32, 1234);
+    printf("a == %i (okay? %s)\n", Value(a), a.is_okay ? "yes" : "no");
 
-/*    Either a = Okay(i32, 1234);
-    printf("a == %i (okay? %s)\n", a.as.i32, a.is_okay ? "yes" : "no");
-//    printf("Value = %i\n", Value(a));
+    Either(chr) b = Err(chr, 'x');
+    printf("b == %c (okay? %s)\n", Value(b), b.is_okay ? "yes" : "no");
 
-    Either e = Okay(str, "awoo");
-    printf("e.type = %lu\n", sizeof(e.type));
-    printf("e == %s\n", Value(e));
-
-    Either b = Err(i32, -1);
-    printf("b == %i   (okay? %s)\n", b.as.i32, b.is_okay ? "yes" : "no");
-*/
     return 0;
 }
