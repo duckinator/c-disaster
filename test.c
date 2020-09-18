c039cb9fc0e5f7e9d6f1d06c5eceb77c662bfdfa
Ellen Marie Dash <me@duckie.co>
2020-09-18 17:02:17 -0400

    eh?

--- test.c
+++ test.c
@@ -1,15 +1,15 @@
-#include <stdbool.h>
-#include <stdint.h>
+//usr/bin/env clang -S -emit-llvm $0 -o- | lli; exit $?
+
 #include <stdio.h>
 
-#define Result(TYPE) struct { TYPE value; bool is_ok; bool is_err; }
+#define Result(TYPE) struct { TYPE value; int is_ok; int is_err; }
 #define Ok(VALUE) {.value=VALUE, .is_ok=1, .is_err=0}
 #define Err(VALUE)  {.value=VALUE, .is_ok=0, .is_err=1}
 #define Value(VALUE) (VALUE.value)
 
 int main()
 {
-    Result(int32_t) a = Ok(1234);
+    Result(int) a = Ok(1234);
     printf("a == %i (okay? %s; error? %s)\n", Value(a),
             a.is_ok ? "yes" : "no",
             a.is_err? "yes": "no");
